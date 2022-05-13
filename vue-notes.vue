49ccaebe8caf4f40f015d60d6b2152fdbb52bc94
kingbloopy <kingbloopy@gmail.com>
2022-05-13 13:15:24 -0700

    vue notes

---
+++ vue-notes.vue
@@ -0,0 +1,46 @@
+<div>
+
+<template>
+  <div class="test">
+    <input type="text" v-model="title"><br/></>
+    <h1>{{ title }}</h1>
+    <p v-if="showName">{{user.firstName}}</p>
+    <p v-else>Nobody</p>
+    <ul>
+      <li v-for="item in items">{{item.title}}</li>
+    </ul>
+    <button v-on:click="greet('Hello Wrld">Say Greeting</button>
+    <input type="text" v-on:keyup="pressKey" />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'test',
+    data() {
+      return {
+        title: 'Hello World',
+        user: {
+          firstName: 'John',
+          lastName: 'Doe'
+        },
+        showName: true,
+        items: [
+          {title: 'Item one'},
+          {title: 'Item two'},
+          {title: 'Item three'}
+        ]
+      }
+    },
+    methods: {
+      greet: function(greeting){
+        alert(greeting);
+      },
+      pressKey: function(e){
+        console.log(e.target.value);
+      }
+    }
+  }
+</script>
+
+</div>
